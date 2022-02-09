f3df26a14b09afd7ee0dafc455c2cefe3261a048
Patrick May <90012596+patrick-may@users.noreply.github.com>
2022-02-09 13:42:26 -0500

    Trimmed and updated documentation

--- Final_Conest/musical_trees.cpp
+++ Final_Conest/musical_trees.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     int trees, people;
-
+    // Reading input
     cin >> people >> trees;
     vector<int> p_loc;
     vector<int> t_loc;
@@ -21,37 +21,38 @@ int main() {
         t_loc.push_back(temp);
     }
 
+    //p_loc is a vector of all locations of people,
+    //t_loc is a vector of all tree locations
     sort(p_loc.begin(), p_loc.end());
     sort(t_loc.begin(), t_loc.end());
     
+    //counter for how many people do not get a tree
     int none = 0;
     
+    //iterate through every tree, assigning it a person
     for(int x = 0; x < trees; ++x) {
-        
+        //people will always go for either tree on "left" or tree on "right"
         int t_loc_one = t_loc[x];
         int t_loc_two = t_loc[x+1];
         int tree_attacker = 0;
 
-       // cout << "LEFT TREE: " << t_loc_one << endl;
-        //cout << "RIGHT TREE: " << t_loc_two << endl;
-
+       //find how many people are going for first tree (tree 1)
         for(int y = 0; y < p_loc.size(); ++y) {
             int person_loc = p_loc[y];
             int r_dist, l_dist;
             l_dist = abs(person_loc - t_loc_one);
             r_dist = abs(person_loc - t_loc_two);
-            //cout << "person " << y << "location " << person_loc << endl;
-            //cout << "left tree dist: " << l_dist << endl;
-            //cout << "right tree_dist: " << r_dist << endl;
+            
+            
             if(l_dist <= r_dist && person_loc != -1){
                 tree_attacker++;
-                //cout << "someone going for tree " << x << endl;
+                
                 p_loc[y] = -1;
                 
             }
             
         }
-        
+        //if more than 1 person is going for the tree, then everyone except 1 person is left without a tree
         if(tree_attacker > 1){
             none += tree_attacker-1;
         }
