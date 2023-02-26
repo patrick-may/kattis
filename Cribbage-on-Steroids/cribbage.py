244fdbdc1980f48f79d4339d3068bb97897e89ae
patrick-may <pmay24@wooster.edu>
2023-02-26 14:16:32 -0500

    I hate dynamic programming

--- Cribbage-on-Steroids/cribbage.py
+++ Cribbage-on-Steroids/cribbage.py
@@ -1,12 +1,41 @@
 from sys import stdin
 from collections import Counter
 from math import comb
-
+from functools import lru_cache
 order = "A23456789TJQK"
 # take in a frequency dict and count the number of possible ways
 # to sum to 15
+# dynamic programming jawn
 def count_15s(n):
-    pass
+    worth = {"A":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"T":10,"K":10,"J":10,"Q":10}
+    point_list = []
+    for card, freq in n.items():
+        point_list.extend([worth[card]]*freq)
+    #print(point_list)
+
+    # cache the call (free dynamic prog?!)
+    @lru_cache(maxsize=None)
+    def recur(sublist, target):
+        local_sum = 0
+
+        # no more possible sums in the entire call branch
+        if sum(sublist) < target:
+            return 0
+        
+        for x, item in enumerate(sublist):
+            if item > target:
+                break
+
+            if item == target:
+                local_sum += 1
+            
+            else:
+                local_sum += recur(sublist[x + 1:], target - item)
+        
+        return local_sum
+            
+    soln = recur(tuple(point_list), 15)
+    return 2 * soln
 
 # take in a frequency dictionary and count the number of pairs that are
 # able to be constructed
@@ -18,14 +47,36 @@ def pairs(n):
             soln += comb(freq, 2)
     return 2*soln
 
+
+def calc_run(f_map):
+
+    if len(f_map.keys()) < 3:
+        return 0
+    
+    multiplier = 1
+    for card, freq in f_map.items():
+        multiplier *= freq
+    
+    #print("run on",f_map,"is",len(f_map.keys())*multiplier)
+    return len(f_map.keys())*multiplier
+    
 # recall we can reorder the cards as desired
 def find_runs(n):
     soln = 0
     run = {}
-    for x, key in enumerate(n.keys()):
+    seq = list(n.keys())
+    for x, key in enumerate(seq):
         run[key] = n[key]
 
-        if n[]
+        if x + 1 > len(seq) - 1:
+            soln += calc_run(run)
+            break
+        if order.index(key) + 1 < order.index(seq[x + 1]):
+            soln += calc_run(run)
+            run = {}
+
+    return soln
+
 
 
 # here lies the I/O and getting stuff running
@@ -40,8 +91,10 @@ for line in stdin:
 hand = sorted(hand, key= lambda e: order.index(e[0]))
 f_map = Counter(hand)
 
-if True:
+if False:
     print(hand)
-    print("15s", count_15s(hand))
+    print("15s", count_15s(f_map))
     print("pairs", pairs(f_map))
-    print("runs",runs(hand))
+    print("runs",find_runs(f_map))
+
+print(count_15s(f_map) + pairs(f_map) + find_runs(f_map))
