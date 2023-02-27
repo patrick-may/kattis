8f7602430c9ca61b77064691f60ceeca2da3304a
patrick-may <pmay24@wooster.edu>
2023-02-26 20:39:15 -0500

    lis kinda dood

--- Alphabet/alphabet.py
+++ Alphabet/alphabet.py
@@ -1,16 +1,34 @@
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+"""
+def simple(s):
+    found = 1
+    after = alphabet[alphabet.find(s[0])+1:]
+
+    for let in s[1:]:
+        if let in after:
+            found += 1
+            after = after[after.find(let)+1:]
+        else:
+            return 26 - found
+
+    return 26 - found
+"""
+
+from functools import lru_cache
 # small case size means brute force could work here...
 case = input()
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-best = 26
-for x, startpoint in enumerate(case):
-    found = 1
-    after = alphabet[alphabet.find(startpoint)+1:]
-    for after_lets in case[x+1:]:
-        c = after.find(after_lets)
-        if c == -1:
-            break
-        after = after[c + 1:]
-        found += 1
-    best = min(best, 26 - found)
+
+# simple longest increasing substring adaptation
+@lru_cache(maxsize=None)
+def lis(case, idx, prev):
+    if idx == len(case):
+        return 0
+
+    take, dontTake = 0, lis(case, idx + 1, prev)
+    if(case[idx] > prev):
+        take = 1 + lis(case, idx + 1, case[idx])
+    return max(take, dontTake)
     
-print(best)
+print(26 - lis(case, 0, chr(ord('a') - 1)))
