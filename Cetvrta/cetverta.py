c332aa8a2a084c93dbc0b71847289e8e38b061b8
patrick-may <pmay24@wooster.edu>
2023-02-28 15:24:22 -0500

    wip

--- Cetvrta/cetverta.py
+++ Cetvrta/cetverta.py
@@ -1,3 +1,8 @@
+import math
 a1, b1 = map(int, input().split())
 a2, b2 = map(int, input().split())
 a3, b3 = map(int, input().split())
+
+abdist = math.sqrt((a1 - a2)**2 + (b1-b2)**2)
+bcdist = math.sqrt((a2-a3)**2 + (b2-b3)**2)
+print(abdist,bcdist)
