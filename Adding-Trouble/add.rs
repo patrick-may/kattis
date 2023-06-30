d176b2dfd17e4187e4beabc6c6ead5031d9655bf
patrick-may <pmay24@wooster.edu>
2023-06-30 13:23:03 -0400

    lets get crusty

---
+++ Adding-Trouble/add.rs
@@ -0,0 +1,21 @@
+
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Couldn't read from stdin");
+  
+    let mut iter = buffer.split_whitespace();
+    let x: i32 = iter.next().unwrap().parse().unwrap();
+    let y: i32 = iter.next().unwrap().parse().unwrap();
+    let z: i32 = iter.next().unwrap().parse().unwrap();
+
+    if x + y == z {
+        print!("correct!");
+    }
+    else {
+        print!("wrong!");
+    }
+}
