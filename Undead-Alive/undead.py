067cbf136ed7afb0af60428e0419657704b67e73
patrick-may <pmay24@wooster.edu>
2023-06-30 13:23:27 -0400

    the simple problems. hush

---
+++ Undead-Alive/undead.py
@@ -0,0 +1,9 @@
+line = input()
+if ":(" in line and ":)" in line:
+    print("double agent")
+elif ":)" in line:
+    print("alive")
+elif ":(" in line:
+    print("undead")
+else:
+    print("machine")
