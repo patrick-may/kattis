75330ebeb31beeb3f71fbc6a0f2dd6b6eac70744
patrick-may <pmay24@wooster.edu>
2023-07-02 11:45:04 -0400

    Sunday Kattis

--- Cetvrta/cetverta.py
+++ Cetvrta/cetverta.py
@@ -3,6 +3,12 @@ a1, b1 = map(int, input().split())
 a2, b2 = map(int, input().split())
 a3, b3 = map(int, input().split())
 
-abdist = math.sqrt((a1 - a2)**2 + (b1-b2)**2)
-bcdist = math.sqrt((a2-a3)**2 + (b2-b3)**2)
-print(abdist,bcdist)
+from collections import Counter
+
+f1 = Counter([a1, a2, a3])
+f2 = Counter([b1, b2, b3])
+
+smallest = sorted(f1.items(), key=lambda x: x[1])[0]
+smallest2 = sorted(f2.items(), key=lambda x: x[1])[0]
+print(smallest[0], smallest2[0])
+# the weirdest solution. I'm sure i'm missing the easy way to do this..
