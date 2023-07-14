8ebb89acc6f7cf511da4f79b92683a4e13a496d5
patrick-may <pmay24@wooster.edu>
2023-07-14 15:07:56 -0400

    formatting

--- Quickscope/2quickscope.cpp
+++ Quickscope/2quickscope.cpp
@@ -3,82 +3,80 @@
 using namespace std;
 
 typedef struct Quickscoper {
-  map<string, vector<string>> variables;
-  stack<set<string>> scope;
+    map<string, vector<string>> variables;
+    stack<set<string>> scope;
 
-  Quickscoper() {
-    set<string> a;
-    scope.push(a);
-  }
-  void step_in() {
-    set<string> a;
-    scope.push(a);
-  }
-
-  void step_out() {
-
-    for (auto key : scope.top()) {
-      variables[key].pop_back();
+    Quickscoper() {
+        set<string> a;
+        scope.push(a);
     }
-    scope.pop(); // remove last frame
-  }
-
-  void declare(string ident, string type) {
-    if (scope.top().count(ident)) {
-      cout << "MULTIPLE DECLARATION\n";
-      exit(0);
+    void step_in() {
+        set<string> a;
+        scope.push(a);
     }
 
-    variables[ident].push_back(type);
-    scope.top().insert(ident);
-  }
-
-  void type_of(string ident) {
-
-    if (variables.count(ident) && variables[ident].size() > 0) {
-      cout << *variables[ident].rbegin() << "\n";
-      return;
+    void step_out() {
+        for (auto key : scope.top()) {
+            variables[key].pop_back();
+        }
+        scope.pop();  // remove last frame
     }
 
-    cout << "UNDECLARED\n";
-    return;
-  }
+    void declare(string ident, string type) {
+        if (scope.top().count(ident)) {
+            cout << "MULTIPLE DECLARATION\n";
+            exit(0);
+        }
 
-  void printqs() {
-    cout << "Map:\n";
-    for (auto p : variables) {
-      cout << p.first << "\n";
-      for (auto v : p.second) {
-        cout << "\t" << v << "\n";
-      }
+        variables[ident].push_back(type);
+        scope.top().insert(ident);
     }
-  }
+
+    void type_of(string ident) {
+        if (variables.count(ident) && variables[ident].size() > 0) {
+            cout << *variables[ident].rbegin() << "\n";
+            return;
+        }
+
+        cout << "UNDECLARED\n";
+        return;
+    }
+
+    void printqs() {
+        cout << "Map:\n";
+        for (auto p : variables) {
+            cout << p.first << "\n";
+            for (auto v : p.second) {
+                cout << "\t" << v << "\n";
+            }
+        }
+    }
 
 } Quickscoper;
 
 int main() {
-  int lines;
-  cin >> lines;
-  Quickscoper qs = Quickscoper();
-  for (; lines > 0; --lines) {
-    string command;
-    cin >> command;
-    if (command == "{") {
-      qs.step_in();
+    int lines;
+    cin >> lines;
+    Quickscoper qs = Quickscoper();
+    for (; lines > 0; --lines) {
+        string command;
+        cin >> command;
+        if (command == "{") {
+            qs.step_in();
 
-    } else if (command == "}") {
-      qs.step_out();
+        } else if (command == "}") {
+            qs.step_out();
 
-    } else if (command == "TYPEOF") {
-      string a;
-      cin >> a;
-      qs.type_of(a);
+        } else if (command == "TYPEOF") {
+            string a;
+            cin >> a;
+            qs.type_of(a);
 
-    } else if (command == "DECLARE") {
-      string a, b;
-      cin >> a >> b;
-      qs.declare(a, b);
+        } else if (command == "DECLARE") {
+            string a, b;
+            cin >> a >> b;
+            qs.declare(a, b);
+        }
+        // qs.printqs();
     }
-    // qs.printqs();
-  }
 }
--- Quickscope/getnoscoped.cpp
+++ Quickscope/getnoscoped.cpp
@@ -3,64 +3,63 @@
 using namespace std;
 
 typedef struct Quickscoper {
-  map<string, string> variables;
-  stack<string> scope;
+    map<string, string> variables;
+    stack<string> scope;
 
-  void step_in() { scope.push("{"); }
+    void step_in() { scope.push("{"); }
 
-  void step_out() {
-
-    while (scope.top() != "{") {
-      variables.erase(scope.top());
-      scope.pop();
-    }
-    scope.pop(); // remove the {
-  }
-
-  void declare(string ident, string type) {
-    if (variables.count(ident)) {
-      cout << "MULTIPLE DECLARATION\n";
-      exit(0);
+    void step_out() {
+        while (scope.top() != "{") {
+            variables.erase(scope.top());
+            scope.pop();
+        }
+        scope.pop();  // remove the {
     }
 
-    variables[ident] = type;
-    scope.push(ident);
-  }
+    void declare(string ident, string type) {
+        if (variables.count(ident)) {
+            cout << "MULTIPLE DECLARATION\n";
+            exit(0);
+        }
 
-  void type_of(string ident) {
-    if (variables.count(ident)) {
-      cout << variables[ident] << "\n";
-      return;
+        variables[ident] = type;
+        scope.push(ident);
     }
 
-    cout << "UNDECLARED\n";
-    return;
-  }
+    void type_of(string ident) {
+        if (variables.count(ident)) {
+            cout << variables[ident] << "\n";
+            return;
+        }
+
+        cout << "UNDECLARED\n";
+        return;
+    }
 
 } Quickscoper;
 
 int main() {
-  int lines;
-  cin >> lines;
-  Quickscoper qs;
-  for (; lines > 0; --lines) {
-    string command;
-    cin >> command;
-    if (command == "{") {
-      qs.step_in();
+    int lines;
+    cin >> lines;
+    Quickscoper qs;
+    for (; lines > 0; --lines) {
+        string command;
+        cin >> command;
+        if (command == "{") {
+            qs.step_in();
 
-    } else if (command == "}") {
-      qs.step_out();
+        } else if (command == "}") {
+            qs.step_out();
 
-    } else if (command == "TYPEOF") {
-      string a;
-      cin >> a;
-      qs.type_of(a);
+        } else if (command == "TYPEOF") {
+            string a;
+            cin >> a;
+            qs.type_of(a);
 
-    } else if (command == "DECLARE") {
-      string a, b;
-      cin >> a >> b;
-      qs.declare(a, b);
+        } else if (command == "DECLARE") {
+            string a, b;
+            cin >> a >> b;
+            qs.declare(a, b);
+        }
     }
-  }
 }
