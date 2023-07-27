281a875a7975f24e2eb280ecbc53d8aba1e57697
patrick-may <pmay24@wooster.edu>
2023-07-26 22:16:46 -0400

    daily commits don't stop 💪

---
+++ Grading/grading.py
@@ -0,0 +1,15 @@
+a, b, c, d, e = map(int, input().split())
+g = int(input())
+
+if g >= a:
+    print("A")
+elif g >= b:
+    print("B")
+elif g >= c:
+    print("C")
+elif g >= d:
+    print("D")
+elif g >= e:
+    print("E")
+else:
+    print("F")
