41feee749c7f722e277c31ae44bf61b095cd81ad
patrick-may <pmay24@wooster.edu>
2023-07-31 22:49:31 -0400

    bruh. numbers oob schit

--- Dejavu/dejavu.cpp
+++ Dejavu/dejavu.cpp
@@ -3,19 +3,17 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
     int ct;
     cin >> ct;
     vector<pair<int, int>> points(ct);
-    map<int, vector<int>> xlookup;
-    map<int, vector<int>> ylookup;
+    map<int, int> xlookup;
+    map<int, int> ylookup;
     for (int i = 0; i < ct; ++i) {
         int x, y;
         cin >> x >> y;
         points[i] = {x, y};
-        xlookup[x].push_back(y);
-        ylookup[y].push_back(x);
+        ++xlookup[x];
+        ++ylookup[y];
     }
 
     long int soln = 0;
@@ -23,18 +21,9 @@ int main() {
         int currx = p.first;
         int curry = p.second;
 
-        int xct = 0;
-        for (auto xmatch : xlookup[currx]) {
-            if (xmatch != curry) {
-                ++xct;
-            }
-        }
-        int yct = 0;
-        for (auto ymatch : ylookup[curry]) {
-            if (ymatch != currx) {
-                ++yct;
-            }
-        }
+        long xct = xlookup[currx] - 1;
+        long yct = ylookup[curry] - 1;
+
         soln += xct * yct;
         // cout << currx << " " << curry << " " << soln << "\n";
     }
