0e4d72ff86921a24932ad20cb14579e29a8b00f3
patrick-may <pmay24@wooster.edu>
2023-08-03 19:55:24 -0400

    ❌INCOMPLETE

---
+++ Vicious-Pikeman-Easy/easypike.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define MODNUM 1000000007
+using namespace std;
+
+vector<int> lengths(int start, int a, int b, int c, int N) {
+    vector<int> soln(N);
+    soln[0] = start;
+    for (int i = 1; i < N; ++i) {
+        soln[i] = (a * soln[i - 1] + b) % c + 1;
+    }
+    return soln;
+}
+int main() {
+    // we can just brute force this, yeah?
+    int N, T;
+    cin >> N >> T;
+    int a, b, c, t0;
+    cin >> a >> b >> c >> t0;
+    vector<int> speeds = lengths(t0, a, b, c, N);
+
+    sort(speeds.begin(), speeds.end());
+    int completed = 0, penalty = 0, prior = 0;
+    for (const auto finprob : speeds) {
+        if (finprob > T) {
+            break;
+        }
+        ++completed;
+        T -= finprob;
+        prior += finprob;
+        prior %= MODNUM;
+        penalty += (prior);
+        penalty %= MODNUM;
+    }
+
+    cout << completed << " " << penalty << "\n";
+    return 0;
+}
