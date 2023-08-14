6de335e3bc1c9d0af9a04dacf31e82e0bf3d9d30
patrick-may <pmay24@wooster.edu>
2023-08-14 18:29:42 -0400

    optimized

--- Font/font.py
+++ Font/font.py
@@ -1,28 +1,30 @@
 from sys import stdin
 
-stdin.readline()
+ct = int(stdin.readline())
 
-words = []
-for _ in stdin.readlines():
-    words.append(set(_.strip()))
+words = [0] * ct
 
-haystack = set('abcdefghijklmnopqrstuvwxyz')
+for idx in range(ct):
+    words[idx] = set(stdin.readline().strip())
+
+haystack = set("abcdefghijklmnopqrstuvwxyz")
+
 
 def backtrack(need, index):
-    
     if len(need) == 0:
-        return 1
+        return 2 ** (len(words) - index)
 
     if index >= len(words):
         return 0
 
-   # take
+    # take
     takect = backtrack(need - words[index], index + 1)
 
     # don't take
-    dontct = backtrack(need, index + 1) 
+    dontct = backtrack(need, index + 1)
 
     return takect + dontct
+
 
 soln = backtrack(haystack, 0)
 print(soln)
