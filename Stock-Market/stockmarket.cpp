9a70927a5dfa94dbe55ff1130f4a1c16955d724a
patrick-may <pmay24@wooster.edu>
2023-08-18 16:34:14 -0400

    greedy algorithm. revisit

---
+++ Stock-Market/stockmarket.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n; double q;
+	cin >> n >> q;
+	
+	vector<double> prices(n);
+	for(int i = 0; i < n; ++i){
+		cin >> prices[i];
+	}
+
+	double money = 100;
+	double shares = 0;
+	
+}
+ 
